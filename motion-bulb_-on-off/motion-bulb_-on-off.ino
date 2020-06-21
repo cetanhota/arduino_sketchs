11abf076a752be24b2d46b6d2492014cf2395350
auser <wayne.leutwyler@gmail.com>
2020-06-21 12:41:20 -0400

    updated PIR initial state.

--- motion-bulb_-on-off/motion-bulb_-on-off.ino
+++ motion-bulb_-on-off/motion-bulb_-on-off.ino
@@ -6,17 +6,18 @@ void setup(){
   pinMode(sensor_pin, INPUT);   // Declared motion sensor pin as Input
   pinMode(relay_pin, OUTPUT);   // Declared relay module pin as Output pin
   // Make the relay module initial state as low, Relay works opposite
+  // had to adjust this code from defaul to HIGH to LOW. 
   digitalWrite(relay_pin, LOW);
 }
 void loop(){
   // Read the output state of motion sensor
   output = digitalRead(sensor_pin);
-  
+
   // If output is High
   if(output == 1){
     digitalWrite(relay_pin, HIGH);
   }
-  
+
   // If output is LOW
   else if(output == 0){
     digitalWrite(relay_pin, LOW);
